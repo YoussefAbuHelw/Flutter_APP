6d98d3dc323627c56ba0d837781c8c79fbc75870
Youssef Mohamed <youssefmohamedseng@gmail.com>
2025-05-11 01:41:04 +0300

    refactor the Code

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -8,7 +8,8 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:sectiontasks/main.dart';
+import 'package:sectiontasks/Entry_Point/main.dart';
+
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
